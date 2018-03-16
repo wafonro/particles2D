d7294bf92e461456ca153cef261734d88961d2e6
Wagner Rodrigues <wagner.fonseca.rodrigues@gmail.com>
2018-03-16 17:38:45 +0100

    deleted test

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,20 @@
 
 import javafx.application.Application;
 import javafx.event.EventHandler;
+import javafx.geometry.Pos;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.scene.shape.Circle;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
@@ -19,28 +27,27 @@ public class Main extends Application {
 
 	}
     public void start(Stage stage) {
-    	int NMAX = 10;
+    	/*int NMAX = 100;
 		Random rand = new Random();
-	    Pane canvas = new Pane();    	
+	    // canvas where particles will be drawn
+		Pane canvas = new Pane();   
+	    
 	    Circle[] circles = new Circle[NMAX];
 	    massParticle[] particles = new massParticle[NMAX];
     	for(int i = 0; i < NMAX; i++) {
-    		circles[i] = new Circle(rand.nextInt(800)+1,rand.nextInt(800)+1, 1);
-    		particles[i] = new massParticle(1, new Vector2D(rand.nextInt(800)+1,rand.nextInt(800)+1), new Vector2D(rand.nextInt(1),rand.nextInt(1)));
+    		circles[i] = new Circle();
+    		particles[i] = new massParticle(rand.nextInt(200)+1, new Vector2D(rand.nextInt(800)+1,rand.nextInt(800)+1), new Vector2D(rand.nextInt(10)-5,rand.nextInt(10)-5));
             canvas.getChildren().add(circles[i]);
     	}
     	Thread updateCircle = new Thread(new Runnable(){
 
 			@Override
 			public void run() {
-				// TODO Auto-generated method stub
-				while(true) {
-					
+				// update loop runs at 60 fps
+				while(true) {			
 					try {
-						Thread.sleep(40);
+						Thread.sleep(1000/60);
 					} catch (InterruptedException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
 					}
 					for(int i = 0; i < NMAX; i++) {
 						particles[i].acceleration = new Vector2D(0.0,0);
@@ -49,9 +56,10 @@ public class Main extends Application {
 							particles[i].acceleration = particles[i].acceleration.sum(particles[i].gravitationalForce(particles[j])).multiply(1/particles[i].mass);						
 						}
 						particles[i].updatePosition(1);
-						particles[i].updateVelocity(10);
+						particles[i].updateVelocity(1);
 						circles[i].setCenterY(particles[i].position.getY());
 						circles[i].setCenterX(particles[i].position.getX());
+						circles[i].setRadius(particles[i].mass/20);
 					}
 				}
 			}
@@ -61,7 +69,7 @@ public class Main extends Application {
         Scene scene = new Scene(canvas, 800, 800);
         stage.setTitle("Circle");
         stage.setScene(scene);
-        stage.show();
+        stage.show();*/
     }
 
 }
