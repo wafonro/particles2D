92908a084311a33f8e40f0698ee070e777ee39fb
sinistro25 <wagner.fonseca.rodrigues@gmail.com>
2018-03-19 15:03:24 +0100

    Changed main to use progressers

--- src/ManagerSimulation.java
+++ src/ManagerSimulation.java
@@ -11,7 +11,7 @@ public class ManagerSimulation implements Runnable{
 	}
 	@Override
 	public void run() {
-		int n_of_threads = 1;
+		int n_of_threads = 10;
 		int n_of_steps = 10000;
 		Force f = new Gravity();
 		double min_mass = 1;
@@ -25,51 +25,20 @@ public class ManagerSimulation implements Runnable{
 		for(int i = 0; i < n_of_particles; i++){
 			particles.add(new Particle(randomGenerator,min_mass, max_mass,max_position,max_velocity,i));
 		}
+		bufferParticles.add(particles);
 		DynSystem system = new DynSystem(particles,delta);
 
 		int n_particles_per_thread = n_of_particles / n_of_threads;
 
 		while(true){
 			Thread[] progresserThreads = new Thread[n_of_threads];
-			Thread[] updateAccelerationThreads = new Thread[n_of_threads];
-			Thread[] updatePositionAndSpeedThreads = new Thread[n_of_threads];
 			for(int k = 0; k < n_of_threads-1; k++){
-				progresserThreads[k] = new Thread(new Progresser(system,k*n_particles_per_thread,(k+1)*n_particles_per_thread,particles));
-				//updateAccelerationThreads[k] = new Thread(new AccelerationUpdater(k*n_particles_per_thread,(k+1)*n_particles_per_thread,particles,f));
-				//updatePositionAndSpeedThreads[k] = new Thread(new PositionAndSpeedUpdater(k*n_particles_per_thread,(k+1)*n_particles_per_thread,particles,delta));
-				
+				progresserThreads[k] = new Thread(new Progresser(system,k*n_particles_per_thread,(k+1)*n_particles_per_thread,bufferParticles));	
 			}
-			progresserThreads[n_of_threads-1] = new Thread(new Progresser(system,(n_of_threads-1)*n_particles_per_thread,n_of_particles,particles));
+			progresserThreads[n_of_threads-1] = new Thread(new Progresser(system,(n_of_threads-1)*n_particles_per_thread,n_of_particles,bufferParticles));
 			for(int j = 0; j < n_of_threads; j++){
 				progresserThreads[j].start();
 			}
-			/*updateAccelerationThreads[n_of_threads-1] = new Thread(new AccelerationUpdater((n_of_threads-1)*n_particles_per_thread,n_of_particles,particles,f));
-			updatePositionAndSpeedThreads[n_of_threads-1] = new Thread(new PositionAndSpeedUpdater((n_of_threads-1)*n_particles_per_thread,n_of_particles,particles,delta));
-			// update acceleration
-			for(int j = 0; j < n_of_threads; j++){
-				updateAccelerationThreads[j].start();
-			}
-			// wait for all processes to finish
-			for(int j = 0; j < n_of_threads; j++){
-				try {
-					updateAccelerationThreads[j].join();
-				} catch (InterruptedException e) {
-					e.printStackTrace();
-				}
-			}
-			bufferParticles.add(particles);
-			// update velocity and position
-			for(int j = 0; j < n_of_threads; j++){
-				updatePositionAndSpeedThreads[j].start();
-			}
-			// wait for all process to finish
-			for(int j = 0; j < n_of_threads; j++){
-				try {
-					updatePositionAndSpeedThreads[j].join();
-				} catch (InterruptedException e) {
-					e.printStackTrace();
-				}
-			}*/
 		}
 	}
 }
